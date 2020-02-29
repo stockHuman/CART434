7d5010925f6eee337d3c2221934b7a581c757f83
Michael Hemingway <mpghemingway@gmail.com>
2020-02-28 23:41:32 -0500

    Placeholder copy

--- src/components/layout/Intro.js
+++ src/components/layout/Intro.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default () => {
-	return (
-		<section id="intro"></section>
-	)
-}
+export default () => (
+	<section id="intro">
+		<div><h1 className="title">Working title</h1></div>
+		<div>ndard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</div>
+		<button>button</button>
+	</section>
+)
--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default () => {
 	return (
 		<nav id="menu" role="navigation" aria-label="Main">
+			<ul>
+				<li><a href="#about">about</a></li>
+			</ul>
 		</nav>
 	)
 }
