bdfd9594fc5a79f4483a7eca9ea1dd7d2da90dcd
Michael Hemingway <mpghemingway@gmail.com>
2020-02-29 23:49:00 -0500

    Additional scaffholding

--- src/components/layout/Intro.js
+++ src/components/layout/Intro.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default () => (
 	<section id="intro">
-		<div><h1 className="title">Working title</h1></div>
-		<div>ndard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</div>
+		<h1 className="title">Working title</h1>
+		<p className="description">ndard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</p>
 		<button>button</button>
 	</section>
 )
