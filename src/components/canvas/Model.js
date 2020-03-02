d80debd4681a1a8d85eae071c0a5a2fa59a325c5
Michael Hemingway <mpghemingway@gmail.com>
2020-03-02 12:09:28 -0500

    Optimised performance and added DOM label

--- src/components/canvas/Model.js
+++ src/components/canvas/Model.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import * as CANNON from 'cannon'
 import { Geometry } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useLoader } from 'react-three-fiber'
+import { useLoader, Dom } from 'react-three-fiber'
 
 import { useCannon } from './useCannon'
 
@@ -11,7 +11,7 @@ import { useCannon } from './useCannon'
  * @param {string} url The path to a .glb, .gltf file to load
  * @param {Vector3} position where to initially place the model
  */
-export default function Model({ url, position, mass = 1000 }) {
+export default function Model({ url, position, mass = 1000, info = 'Doot' }) {
 	const gltf = useLoader(GLTFLoader, url)
 	const shape = Shape(gltf.scene.children[0])
 	const ref = useCannon({ mass }, body => {
@@ -20,39 +20,46 @@ export default function Model({ url, position, mass = 1000 }) {
 	})
 
 	return (
-		<mesh ref={ref} castShadow receiveShadow>
-			<primitive attach="geometry" object={gltf.scene.children[0].geometry} />
-			<meshNormalMaterial attach="material" />
-		</mesh>
+		<group ref={ref}>
+			<Dom className="model-info"><span>{info}</span></Dom>
+			<mesh castShadow receiveShadow>
+				<primitive attach="geometry" object={gltf.scene.children[0].geometry} />
+				<meshNormalMaterial attach="material" />
+			</mesh>
+		</group>
 	)
 }
 
 /**
- *
- * @param  {THREE.Object3D.geometry} geometry
+ * Translates Three object data into a Cannon-compatible mesh
+ * @param  {THREE.Object3D} mesh
  * @return {CANNON.Shape}
  */
 function Shape (mesh) {
-	console.log('called')
-	const geo = new Geometry()
-	geo.fromBufferGeometry(mesh.geometry)
+	const shape = useMemo(() => {
+		const geo = new Geometry()
+		geo.fromBufferGeometry(mesh.geometry)
 
-	// Convert from THREE.Vector3 to CANNON.Vec3.
-	const vertices = new Array(geo.vertices.length)
+		// Convert from THREE.Vector3 to CANNON.Vec3.
+		const vertices = new Array(geo.vertices.length)
 
-	for (let i = 0; i < vertices.length; i++) {
-		vertices[i] = new CANNON.Vec3(
-			geo.vertices[i].x,
-			geo.vertices[i].y,
-			geo.vertices[i].z
-		)
-	}
+		for (let i = 0; i < vertices.length; i++) {
+			vertices[i] = new CANNON.Vec3(
+				geo.vertices[i].x,
+				geo.vertices[i].y,
+				geo.vertices[i].z
+			)
+		}
 
-	// Convert from THREE.Face to Array<number>
-	const faces = new Array(geo.faces.length)
-	for (let i = 0; i < geo.faces.length; i++) {
-		faces[i] = [geo.faces[i].a, geo.faces[i].b, geo.faces[i].c]
-	}
+		// Convert from THREE.Face to Array<number>
+		const faces = new Array(geo.faces.length)
+		for (let i = 0; i < geo.faces.length; i++) {
+			faces[i] = [geo.faces[i].a, geo.faces[i].b, geo.faces[i].c]
+		}
 
-	return new CANNON.ConvexPolyhedron(vertices, faces)
+		// NOTE: non-convex shapes will produce errors in inopportune collisions
+		return new CANNON.ConvexPolyhedron(vertices, faces)
+	}, [mesh])
+
+	return shape
 }
