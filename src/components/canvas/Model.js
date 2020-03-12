17df5323e9daea84dd5c2922114e4d38b377d4c1
Michael Hemingway <mpghemingway@gmail.com>
2020-03-12 17:58:26 -0400

    Use new models

--- src/components/canvas/Model.js
+++ src/components/canvas/Model.js
@@ -1,8 +1,9 @@
 import React, { useMemo } from 'react'
-import { Geometry } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useLoader, Dom } from 'react-three-fiber'
 import { useConvexPolyhedron } from 'use-cannon'
+
+import { getGeometry } from './util/util'
 
 /**
  *
@@ -10,17 +11,15 @@ import { useConvexPolyhedron } from 'use-cannon'
  * @param {Vector3} position where to initially place the model
  */
 export default function Model(props) {
-	const { scene } = useLoader(GLTFLoader, props.url)
-	const args = useMemo(() => {
-		const geo = new Geometry().fromBufferGeometry(scene.children[0].geometry)
-		return [geo.vertices.map(v => [v.x, v.y, v.z]), geo.faces.map(f => [f.a, f.b, f.c])]
-	}, [scene])
-	const [ref] = useConvexPolyhedron(() => ({ mass: 100, ...props, args }))
+	const { nodes } = useLoader(GLTFLoader, props.url)
+	const geo = useMemo(() => getGeometry(nodes.Collider), [nodes])
+	const [ref] = useConvexPolyhedron(() => ({ mass: 100, ...props, args: geo }))
 
 	return (
 		<group ref={ref}>
 			<Dom className="model-info"><span>{props.info}</span></Dom>
-			<mesh castShadow ref={ref} geometry={scene.children[0].geometry} dispose={null}>
+			<mesh castShadow ref={ref} dispose={null}>
+				<primitive object={nodes.Object} attach="geometry" />
 				<meshNormalMaterial attach="material" />
 			</mesh>
 		</group>
