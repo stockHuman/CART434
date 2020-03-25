da2c64c44b8e81de57b25976646cd5bb58681e0e
Michael Hemingway <mpghemingway@gmail.com>
2020-03-25 14:12:34 -0400

    Update Scene to import from data.js

--- src/components/canvas/Scene.js
+++ src/components/canvas/Scene.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { usePlane, Physics } from 'use-cannon'
 
 import Model from './Model'
-
-// 3D models
-import ModelLamp from '../../assets/meshes/MichaelH_Pixies_StreetLamp_MOD.001.glb'
-import ModelBattery from '../../assets/meshes/MichaelH_Pixies_Battery_MOD.000.glb'
-import ModelContainer from '../../assets/meshes/MichaelH_Pixies_Container_MOD.001.glb'
+import data from '../../data'
 
 // via https://codesandbox.io/s/r3f-cannon-instanced-physics-g1s88
 function Floor({ position = [0, 0, 0], rotation = [0, 0, 0] }) {
@@ -30,7 +26,7 @@ export default (props) => {
 		// the app has calculated that such an object should exist
 		for (let i = 0; i < props[property]; i++) {
 			objects.push({
-				url: ModelBattery,
+				url: data.battery.url,
 				info: `12 AA batteries - ${Math.floor(Math.random() * 5)}GWh`,
 				position: [2.5 - Math.random() * 5, Math.random() * 30, Math.random()],
 				key: Math.random()
@@ -39,30 +35,39 @@ export default (props) => {
 	}
 
 	return (
-		<scene>
-			<Physics iterations={10} size={40}>
-				<Floor rotation={[-Math.PI / 2, 0, 0]}  />
-				{ objects.map(object => (
-					<Model
-						url={object.url}
-						position={object.position}
-						info={object.info}
-						key={object.key}
-					/>
-				)) }
+		<Physics iterations={20}>
+			<Floor rotation={[-Math.PI / 2, 0, 0]}  />
+			{ objects.map(object => (
 				<Model
-					url={ModelLamp}
-					position={[3, 5, 0]}
-					info={'Light a small town for a week'}
+					url={object.url}
+					position={object.position}
+					info={object.info}
+					key={object.key}
 				/>
-				<Model
-					url={ModelContainer}
-					position={[5, 10, 0]}
-					rotation={[Math.PI / 4, 0, 2]}
-					mass={200}
-					info={'Light a small town for a week'}
-				/>
-			</Physics>
-		</scene>
+			)) }
+			<Model
+				url={data.lamp.url}
+				position={[1, 15, 0]}
+				rotation={[0, 0, 2]}
+				mass={3}
+				debug
+				info={'Light a small town for a week'}
+			/>
+			<Model
+				url={data.container.url}
+				position={[2, 10, 0]}
+				rotation={[Math.PI / 4, 0, 2]}
+				mass={200}
+				debug
+				info={'Ship A container to China from X'}
+			/>
+			<Model
+				url={data.bean.url}
+				position={[-2, 10, 0]}
+				rotation={[Math.PI, 1, 2]}
+				mass={0.1}
+				info={'Light a small town for a week'}
+			/>
+		</Physics>
 	)
 }
