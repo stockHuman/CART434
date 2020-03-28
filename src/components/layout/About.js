ead819cf026c657e63788ba4d3b9a9d4b1336cc8
Michael Hemingway <mpghemingway@gmail.com>
2020-03-28 18:48:20 -0400

    Add YouTube power source

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default () => {
 	return (
-		<section id="about" role="complementary"></section>
+		<section id="about" role="complementary">
+			<h3>Sources</h3>
+			<ul>
+				<li>YouTube video power usage: <a href="https://sustainabilityillustrated.com/en/2016/04/21/energy-use-youtube-videos/">Minute Earth</a></li>
+				<li></li>
+			</ul>
+
+		</section>
 	)
 }
