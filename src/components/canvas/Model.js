40591846f1a59eb5ce2de4835fc76af3cdc1bed0
Michael Hemingway <mpghemingway@gmail.com>
2020-03-28 18:48:02 -0400

    Improve debugging on Models

--- src/components/canvas/Model.js
+++ src/components/canvas/Model.js
@@ -8,21 +8,25 @@ import { getGeometry, mergeCoplanarFaces } from './util/util'
 export default function Model(props) {
 	const { nodes } = useLoader(GLTFLoader, props.url)
 	const geo = useMemo(() => getGeometry(nodes.Collider || nodes.Object), [nodes])
-	const geo2 = useMemo(() => mergeCoplanarFaces(geo), [geo])
+	// const geo2 = useMemo(() => mergeCoplanarFaces(geo), [geo])
 
 	const [ref] = useConvexPolyhedron(() => ({
 		...props,
 		mass: props.mass || 1,
-		args: geo2
+		args: geo
 	}))
 
 	return (
 		<group ref={ref}>
-			<Dom className="model-info"><span>{props.info}</span></Dom>
-			<mesh receiveShadow castShadow ref={ref}>
-				<primitive object={props.debug ? geo : nodes.Object} attach="geometry" />
-				<meshStandardMaterial attach="material" wireframe />
+			<Dom className="model-info"><span>{props.info}{props.debug ? <p>DEBUG</p> : null}</span></Dom>
+			<mesh receiveShadow castShadow>
+				<primitive object={nodes.Object} attach="geometry" />
 			</mesh>
+			{props.debug ?
+				<mesh geometry={geo}>
+					<meshBasicMaterial attach="material" wireframe />
+				</mesh>
+			: null}
 		</group>
 	)
 }
