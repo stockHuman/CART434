3761396d510b04c7bb8af423470803b2ed375e79
Michael Hemingway <mpghemingway@gmail.com>
2020-03-28 22:53:55 -0400

    Phase one of full vertical slice: compute shapes with hours

--- src/components/layout/Application.js
+++ src/components/layout/Application.js
@@ -4,10 +4,10 @@ import Viewport from '../canvas/Viewport'
 import Scene from '../canvas/Scene'
 
 export default class Application extends Component {
-
 	constructor (props) {
 		super(props)
 		this.state = {
+			values: {youtube: 0, netflix: 0, facebook: 0, LTE: 0, skype: 0}, // preset or entered values
 			wattage: { value: 0, unit: 'Wh' }, // consumption per annum
 			objects: { // objects that take up that wattage
 				batteries: 0
@@ -15,6 +15,7 @@ export default class Application extends Component {
 		}
 
 		this.computeObjects = this.computeObjects.bind(this)
+		this.changePreset = this.changePreset.bind(this)
 	}
 
 	computeObjects () {
@@ -26,32 +27,45 @@ export default class Application extends Component {
 		})
 	}
 
+	changePreset ({ nativeEvent }) {
+		const v = parseInt(nativeEvent.target.value)
+		switch (v) {
+			case 0:
+				this.setState({ values: { youtube: 3, netflix: 1, facebook: 0, LTE: 0, skype: 0 }})
+				break;
+
+			default:
+				this.setState({ values: { youtube: 0, netflix: 0, facebook: 0, LTE: 0, skype: 0 } })
+				break;
+		}
+	}
+
 	render () {
-		const { wattage, objects } = this.state
+		const { wattage, objects, values } = this.state
 
 		const NumberField = ({field, name, hours = 0}) => (
 			<div className="input-group">
 				<label htmlFor={name}>{field}</label>
-				<input name={name} placeholder={hours} size="2"/>
+				<input min="0" name={name} size="2" type="number" defaultValue={hours}/>
 			</div>
 		)
 
 		return (
 			<section id="app" role="main">
 				<aside id="form">
-					<select name="presets">
-						<option>Presets</option>
-						<option>Heavy Gaming (Online, PC)</option>
-						<option>Light Gaming (Online, PC)</option>
-						<option>Light Gaming (Local, PC)</option>
-						<option>Heavy Gaming (Online, Console)</option>
-						<option>Some YouTube Hours</option>
+					<select name="presets" onChange={this.changePreset}>
+						<option value="-1">Presets</option>
+						<option value="0">Heavy Gaming (Online, PC)</option>
+						<option value="1">Light Gaming (Online, PC)</option>
+						<option value="2">Light Gaming (Local, PC)</option>
+						<option value="3">Heavy Gaming (Online, Console)</option>
+						<option value="4">Some YouTube Hours</option>
 					</select>
-					<NumberField field="Hours on Youtube" name="youtube" />
-					<NumberField field="Hours on NetFlix" name="netflix" />
-					<NumberField field="Hours on Facebook" name="facebook" />
-					<NumberField field="Hours using 4G LTE" name="LTE" />
-					<NumberField field="Hours videoconferencing" name="skype" />
+					<NumberField hours={values.youtube} field="Hours on Youtube" name="youtube" />
+					<NumberField hours={values.netflix} field="Hours on NetFlix" name="netflix" />
+					<NumberField hours={values.facebook} field="Hours on Facebook" name="facebook" />
+					<NumberField hours={values.LTE} field="Hours using 4G LTE" name="LTE" />
+					<NumberField hours={values.skype} field="Hours videoconferencing" name="skype" />
 					<select>
 						<option>Canada</option>
 					</select>
