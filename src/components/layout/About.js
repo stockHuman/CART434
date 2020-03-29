551b67a661b9388dd4b2bdf90584022154a902ba
Michael Hemingway <mpghemingway@gmail.com>
2020-03-29 16:40:21 -0400

    Add sources

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -6,9 +6,9 @@ export default () => {
 			<h3>Sources</h3>
 			<ul>
 				<li>YouTube video power usage: <a href="https://sustainabilityillustrated.com/en/2016/04/21/energy-use-youtube-videos/">Minute Earth</a></li>
-				<li></li>
+				<li>LTE by application: <a href="https://res.mdpi.com/d_attachment/energies/energies-12-00184/article_deploy/energies-12-00184.pdf">Modeling the Total Energy Consumption of Mobile Network Services and Applications</a>, Table 1</li>
+				<li>Additional notes on mobile power consumption (2013)<a href="https://thinkprogress.org/does-your-iphone-use-as-much-electricity-as-a-new-refrigerator-not-even-close-4a5e0ab41a13/"></a></li>
 			</ul>
-
 		</section>
 	)
 }
