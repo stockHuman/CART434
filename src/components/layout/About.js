69da872f1269135c3400a0915f637818a3d42867
Michael Hemingway <mpghemingway@gmail.com>
2020-03-30 10:45:12 -0400

    Add sources

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -9,6 +9,8 @@ export default () => {
 				<li>LTE by application: <a href="https://res.mdpi.com/d_attachment/energies/energies-12-00184/article_deploy/energies-12-00184.pdf">Modeling the Total Energy Consumption of Mobile Network Services and Applications</a>, Table 1</li>
 				<li>Additional notes on mobile power consumption (2013): <a href="https://thinkprogress.org/does-your-iphone-use-as-much-electricity-as-a-new-refrigerator-not-even-close-4a5e0ab41a13/">ThinkProgress</a></li>
 				<li>Streaming on Netflix: <a href="https://www.carbonbrief.org/factcheck-what-is-the-carbon-footprint-of-streaming-video-on-netflix">CarbonBrief</a></li>
+				<li>Insights into website sustainability and server usage (<a href="https://www.lowtechmagazine.com/2020/01/how-sustainable-is-a-solar-powered-website.html">lowtechmagazine</a>)</li>
+				<li>recent article on emissions and energy use of the internet (<a href="https://dannyvankooten.com/website-carbon-emissions">dannyvankooten</a>)</li>
 			</ul>
 		</section>
 	)
