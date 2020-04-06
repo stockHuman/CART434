4de2b06a2296426ab7324808cc72c9088cd738aa
Michael Hemingway <mpghemingway@gmail.com>
2020-04-05 22:53:08 -0400

    Updated name and icon

--- src/components/layout/Intro.js
+++ src/components/layout/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
 	<section id="intro">
-		<h1 className="title">Streams</h1>
+		<h1 className="title">Data Falls</h1>
 		<p className="description">The notion that internet browsing has any carbon impact at all is largely completely foreign to your average netizen.<br />
 Indeed, online browsing, or other computer-related behavior is generally thought of as carbon neutral, with perhaps some minor concern for the carbon impact of shipping and manufacturing the initial components.
 This project aims to educate and illustrate in a jovial manner, the electrical costs running major services entails, and how that is affected by local and international power delivery.</p>
