22b437e3c92b400f76db88afb489263a7fd86aa8
Michael Hemingway <mpghemingway@gmail.com>
2020-04-16 21:12:53 -0400

    restructuring

--- src/components/canvas/Scene.js
+++ src/components/canvas/Scene.js
@@ -5,31 +5,18 @@ import Model from './Model'
 import data from '../../data'
 
 // via https://codesandbox.io/s/r3f-cannon-instanced-physics-g1s88
-function Floor({ position = [0, 0, 0], rotation = [0, 0, 0] }) {
+function Floor({ position = [ 0, 0, 0 ], rotation = [ 0, 0, 0 ] }) {
 	// Register plane as a physics body with zero mass
-	const [ref] = usePlane(() => ({ mass: 0, position, rotation }))
+	const [ ref ] = usePlane(() => ({ mass: 0, position, rotation }))
 	return (
 		<mesh ref={ref} receiveShadow>
-			<planeBufferGeometry attach="geometry" args={[100, 100]} />
+			<planeBufferGeometry attach="geometry" args={[ 100, 100 ]} />
 			<meshStandardMaterial attach="material" color="#BFBFBF" />
 		</mesh>
 	)
 }
 
-// function Meshes ({object, count}) {
-// 	return (
-// 		<instancedMesh args={[null, null, count]}>
-// 			<Model
-// 				url={object.url}
-// 				position={object.position}
-// 				overlay={object.info}
-// 				key={object.key}
-// 			/>
-// 		</instancedMesh>
-// 	)
-// }
-
-export default function Scene (props) {
+export default function Scene(props) {
 	let objects = []
 
 	// template, place objects in array here
@@ -39,45 +26,33 @@ export default function Scene (props) {
 		// the app has calculated that such an object should exist
 		for (let i = 0; i < props[property]; i++) {
 			objects.push({
-				url: data.bean.url,
+				url: data.arm.url,
 				info: `12 AA batteries - ${Math.floor(Math.random() * 5)}GWh`,
-				position: [2.5 - Math.random() * 5, Math.random() * 30, Math.random()],
-				key: Math.random()
+				position: [ 2.5 - Math.random() * 5, Math.random() * 30, Math.random() ],
+				key: Math.random(),
 			})
 		}
 	}
 
 	return (
 		<Physics iterations={10}>
-			<Floor rotation={[-Math.PI / 2, 0, 0]}  />
-			{ objects.map(object => (
-				<Model
-					url={object.url}
-					position={object.position}
-					overlay={object.info}
-					key={object.key}
-				/>
-			)) }
+			<Floor rotation={[ -Math.PI / 2, 0, 0 ]} />
+			{objects.map((object) => (
+				<Model url={object.url} position={object.position} overlay={object.info} key={object.key} />
+			))}
 			<Model
 				url={data.lamp.url}
-				position={[0, 15, 0]}
-				rotation={[0, 0.4, 1]}
+				position={[ 0, 15, 0 ]}
+				rotation={[ 0, 0.4, 1 ]}
 				mass={3}
 				overlay={'Light a small town for a week'}
 			/>
 			<Model
 				url={data.container.url}
-				position={[2, 20, -6]}
-				rotation={[-1, 0.2, 1]}
+				position={[ 2, 20, -6 ]}
+				rotation={[ -1, 0.2, 1 ]}
 				mass={200}
 				overlay={'Ship A container to China from X'}
-			/>
-			<Model
-				url={data.container.url}
-				position={[2, 11, -6]}
-				rotation={[-1, 0.2, 1]}
-				mass={200}
-				overlay={'Ship A container to China'}
 			/>
 		</Physics>
 	)
