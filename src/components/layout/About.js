6afde4eccc5a78b60668edf6fc48fc15e49daeb8
Michael Hemingway <mpghemingway@gmail.com>
2020-04-17 21:45:57 -0400

    Completed About paragraph

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -5,7 +5,10 @@ export default () => {
 		<section id="about" role="complementary">
 			<div className="half">
 				<h3>About</h3>
-				<p>This project was conceived</p>
+				<p>At the heart of this project is my own hope for a greener future, which, perhaps some day, might actually come to be. As someone in the tech world with a personal fondness for environmentalism, I've been increasingly aware of and interested in the carbon footprint the behemoth that the internet has on the world, and how digital habits impact that picture.</p>
+				<p>The aim of this project is, in broad terms, encourage one to see the massive scale that online services live in, and to hopefully take from that not a message of personal culpability, but a notion of interconnectedness between scales large and small. The shipping container, as a 3D model, was specially chosen as that industry alone furnishes all the little tech miracles we enjoy, and contributes massively to both oceanic environmental degradation, invasive speacies proliferation and to global carbon emissions. </p>
+				<p>_</p>
+				<p>Thank you kindly for giving this a look</p>
 			</div>
 			<div className="half">
 			<h3>Sources</h3>
@@ -19,6 +22,7 @@ export default () => {
 				<li>recent article on emissions and energy use of the internet (<a href="https://dannyvankooten.com/website-carbon-emissions">dannyvankooten</a>)</li>
 				<li>Energy used to brew a cup of coffee (<a href="https://business.directenergy.com/blog/2017/september/international-coffee-day">DirectEnergy</a>, using 0.08Wh as an average between drip and espresso)</li>
 				<li>Emma Mærsk energy use per hour <a href="https://en.wikipedia.org/wiki/Emma_M%C3%A6rsk">WikiPedia, Wärtsilä-Sulzer RTA96-C</a></li>
+				<li>Many other minor sources listed within source comments or with supplementary documentation linked in the footer.</li>
 			</ul>
 			</div>
 		</section>
